321822e4863afaf70e770c6b5e467e60ec73d81f
sanikaraut <sanikaraut@microsoft.com>
2023-02-08 22:00:13 -0800

    initial commit

---
+++ server/env/environment.js
@@ -0,0 +1,9 @@
+const cosmosPort = 10255; // replace with your port
+const dbName = 'reactapp-cosmos-db';
+const key = 'pdTF93AiuXm9EeHOAzkuaFcdPZEl5TFugIv3F8rf81yVt5yvHayAJNKJzcshyvIepY2cnR7u9iJgACDb4g1lIw==';
+
+module.exports = {
+  cosmosPort,
+  dbName,
+  key
+};
